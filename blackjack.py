de135efb9803353f9bbcdc93e6b9f0dddc2fd8da
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-06 14:52:54 -0500

    you can now play a game of blackjack (betting and payout not yet fully implemented

--- blackjack.py
+++ blackjack.py
@@ -1,13 +1,37 @@
 from random import shuffle
 
 
+def find_player_bank_account(file1):
+    with open(file1, 'r') as file:
+        money = int(file.readline())
+    return money
+
+
+def place_ya_bets(money):
+    money = find_player_bank_account('bank_account.txt')
+    while True:
+        player_bet_amount = input(
+            'How much do you want to bet? (This is a $20 table; whole dollar amounts only please) '
+        )
+        if player_bet_amount.isdigit():
+            player_bet_amount = int(player_bet_amount)
+            if 20 <= player_bet_amount <= money:
+                return player_bet_amount
+            elif player_bet_amount > money:
+                print(
+                    "You are trying to bet more than you have.  If this was any other casino, I wouldn't be warning you...\n\n"
+                )
+        else:
+            print('Invalid money amount')
+
+
 def build_deck():  #creates the deck
     cards = []
     for i in range(2, 11):
         cards.extend([i] * 4)
     for _ in range(3):
         cards.extend([10] * 4)
-    cards.extend(['A'] * 4)
+    cards.extend(['Ace'] * 4)
     return cards
 
 
@@ -20,7 +44,7 @@ def get_hand_total(hand):  #adds total of deck with ace rules
     non_aces = []
     aces = 0
     for card in hand:
-        if card == 'A':
+        if card == 'Ace':
             aces += 1
         else:
             non_aces.append(card)
@@ -71,12 +95,10 @@ def round_start_dealer(cards):  #starting each round for computer
 
 def input_hit_or_stand():  #hit or stand while loop
     while True:
-        hit_or_stand = input('hit or stand or show?  ').lower()
+        hit_or_stand = input('hit or stand  ').lower()
         if hit_or_stand == 'hit':
             return hit_or_stand
         elif hit_or_stand == 'stand':
-            return hit_or_stand
-        elif hit_or_stand == 'show all':
             return hit_or_stand
         else:
             print('Invalid input')
@@ -86,55 +108,90 @@ def hit_me(player_hand, card):
     player_hand.append(card)
 
 
-def change_ace(player_hand):
-    player_hand.remove(11)
-    player_hand.append(1)
-
-
 def game_play_player(dealer_hand, player_hand, cards):
     while True:
         player_hand_sum = get_hand_total(player_hand)
+        print('\n\nYour cards : {}\n'.format(', '.join(map(str, player_hand))))
+        print('\nDealer has {}, ?\n'.format(dealer_hand[1]))
         if player_hand_sum > 21:
-            print(player_hand)
-            print('bust')
+            print('\nbust')
             return player_hand_sum
         if if_blackjack(player_hand_sum):
+            print('Blackjack!')
             return player_hand_sum
-        if len(cards) < 1:
-            break
         choice = input_hit_or_stand()
         if choice == 'hit':
             hit_me(player_hand, cards.pop(0))
-        elif choice == 'show all':
-            print('Player has {}'.format(player_hand))
-            print('Dealer has [{}, ?]'.format(dealer_hand[1]))
         elif choice == 'stand':
             return player_hand_sum
-        print(len(cards))
 
 
-def game_play_dealer(dealer_hand, cards):
-    while dealer_hand < 21:
+def game_play_dealer(dealer_hand, cards, player_hand_sum):
+    dealer_hand_total = 0
+    while dealer_hand_total <= 21:
         dealer_hand_total = get_hand_total(dealer_hand)
-        if 
+        if dealer_hand_total >= 17 or dealer_hand_total > player_hand_sum:
+            break
+        dealer_hand.append(cards.pop(0))
+    return dealer_hand_total
 
 
-def big_gameplay(shuffled_cards):
-    #if len(shuffled_cards) <= 4:  #if not enough cards to start a game
-    #break
-    player_hand = round_start_player(shuffled_cards)  #deals 2 cards to player
-    dealer_hand = round_start_dealer(shuffled_cards)  #deals 2 cards to dealer
-    print(len(shuffled_cards))
-    player_hand_sum = game_play_player(dealer_hand, player_hand,
-                                       shuffled_cards)
-    dealer_sum = game_play_dealer(dealer_hand, shuffled_cards)
+def winner(player_hand_sum, dealer_hand_sum, player_hand, dealer_hand):
+    player_hand_str = ', '.join(map(str, player_hand))
+    dealer_hand_str = ', '.join(map(str, dealer_hand))
+    if dealer_hand_sum > 21 and player_hand_sum <= 21:
+        print('Dealer has busted with a deck of {}!  Player wins! :D'.format(
+            dealer_hand_str))
+    elif dealer_hand_sum < player_hand_sum <= 21:
+        print(
+            'Player Wins with a hand of {} against a dealer hand of {}'.format(
+                player_hand_str, dealer_hand_str))
+    elif player_hand_sum < dealer_hand_sum <= 21:
+        print('\nDealer wins with a hand of {} against your hand of {}. :('.
+              format(dealer_hand_str, player_hand_str))
+    elif player_hand_sum == dealer_hand_sum:
+        print('push')
+
+
+def blackjack():
+    continue_game = True
+    while continue_game:
+        money = find_player_bank_account('bank_account.txt')
+        player_bet_amount = place_ya_bets(money)
+        money_taken_out = change_bank_account('bank_account.txt',
+                                              player_bet_amount, money)
+        cards = build_deck()
+        shuffled_cards = shuffle_deck(cards)
+        player_hand = round_start_player(
+            shuffled_cards)  #deals 2 cards to player
+        dealer_hand = round_start_dealer(
+            shuffled_cards)  #deals 2 cards to dealer
+        player_hand_sum = game_play_player(dealer_hand, player_hand,
+                                           shuffled_cards)
+        dealer_sum = game_play_dealer(dealer_hand, shuffled_cards,
+                                      player_hand_sum)
+        winner(player_hand_sum, dealer_sum, player_hand, dealer_hand)
+        continue_game = play_again()
+
+
+def change_bank_account(file1, player_bet_amount, money):
+    with open(file1, 'w') as file:
+        file.write(str(money - player_bet_amount))
+
+
+def play_again():
+    while True:
+        continue_player = input('\nPlay again?\n').lower()
+        if 'yes' == continue_player:
+            return True
+        elif 'no' == continue_player:
+            return False
+        else:
+            print('\nInvalid input\n')
 
 
 def main():
-    while True:
-        cards = build_deck()
-        shuffled_cards = shuffle_deck(cards)
-        big_gameplay(shuffled_cards)
+    blackjack()
 
 
 if __name__ == '__main__':
